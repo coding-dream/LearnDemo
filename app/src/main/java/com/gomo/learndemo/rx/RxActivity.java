31e65086d981b0b6a177ec91c02d79baf616cf32
coding-dream <352085768@qq.com>
2018-05-05 23:19:02 +0800

    add rx test

---
+++ app/src/main/java/com/gomo/learndemo/rx/RxActivity.java
@@ -0,0 +1,16 @@
+package com.gomo.learndemo.rx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gomo.learndemo.R;
+
+public class RxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx);
+
+    }
+}
